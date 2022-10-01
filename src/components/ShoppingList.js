540ca4304fbe683baf8903b9d6aa339e3622e577
Francis Muriithi <mufraggie@gmail.com>
2022-10-01 07:16:16 +0300

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,10 +7,35 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+//DISPLAYING. Add the useEffect to fetch the items and display
+    useEffect(()=>{
+      fetch("http://localhost:4000/items")
+       .then((res) => res.json())
+       .then((items)=> setItems(items))
+    })
+
+    //POSTING in order to post the new item without refreshing the page, we add a handleNewItem function on the parent component.
+  function handleNewItem(newItem){
+    setItems ([...items, newItem]);
+  }
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
-
+//Updating
+function handleUpdateItem(updatedItem) {
+  const updatedItems = items.map((item) => {
+    if (item.id === updatedItem.id) {
+      return updatedItem;
+    } else {
+      return item;
+    }
+  });
+  setItems(updatedItems);
+}
+function handleDeleteItem(deletedItem) {
+  const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+}
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
@@ -19,14 +44,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddNewItem ={handleNewItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
